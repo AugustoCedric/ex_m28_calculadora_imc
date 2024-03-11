d81713498b94c71c467af4fd51a07ce54d408fec
Augusto Cedric <cedric.pmi.rmp@gmail.com>
2024-03-11 07:08:24 -0300

    finalização do exercicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import React, { useState, useEffect } from "react";
+import TabelaIMC from "./componentes/Tabela/Index";
+import Perfil from "./componentes/Perfil/index";
+import Formulario from "./componentes/Formulario/index"; // Adicionei a importação do componente Formulario
 
 function App() {
- 
   return (
-    <main>
-      <h1>Calculadora de IMC</h1>
-    </main>
-  )
+    <>
+      <div className="divBlock">
+        <Perfil />
+        <section id="result">
+        <Formulario />
+          <section id="reference-table">
+            <TabelaIMC />
+          </section>
+        </section>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
